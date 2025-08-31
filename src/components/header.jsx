19d35baf454b05ccfb4934bc602017e1fce5f441
Riiskager <emil.axel.riis@gmail.com>
2025-08-31 17:04:21 +0200

    ikon sat ind

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import HjemIkon from '../assets/home.png'
+
+export default function Header(){
+    return(
+        <Fragment>
+            <h1>Velkommen til mit portfolie</h1>
+            <img className="hjemikon" src={HjemIkon} alt="hjem-ikon"/>
+        </Fragment>
+    )
+}
