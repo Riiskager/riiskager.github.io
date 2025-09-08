16a93bd97e974144d81ec52084231471548c0ac6
Riiskager <emil.axel.riis@gmail.com>
2025-09-08 12:05:02 +0200

    nyt shit

--- src/components/TopCat.jsx
+++ src/components/TopCat.jsx
@@ -1,10 +1,16 @@
 import categories from "../Data/Categories";
+import { useState } from "react";
 
 export default function TopCat() {
+    const [selectedIndex, SetSelectedIndex] = useState(0)
+
+    //Return functionen looper gennem categories, og mapper dem alle én af gangen
   return (
-    <ul className="top-categories">
-      {categories.map((category, index) => (
-        <li key={index} className="category-item">
+    //listen laves
+    <ul className="top-categories"> 
+      {categories.map((category, index) => ( //categories mappes
+      //Det gør den med en liste, som får en key
+        <li key={index} className="category-item"> 
           <img src={category.img} alt={category.name + " icon"} />
           {category.name}
         </li>
