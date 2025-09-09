89bad677894b5adc1855e586bb16cba46bf1c70f
Riiskager <emil.axel.riis@gmail.com>
2025-09-09 17:48:02 +0200

    subkategori virker

--- src/Data/Categories.js
+++ src/Data/Categories.js
@@ -2,12 +2,32 @@ import meIcon from '../assets/omMig.svg'
 import grafikIkon from '../assets/grafik.svg'
 import projektIkon from '../assets/projekt.svg'
 import SoMeIkon from '../assets/SoMe.svg'
+import placeholder from '../assets/react.svg'
 
   
   const categories = [
-  { name: "Me", img: meIcon },
-  { name: "SoMe", img: SoMeIkon },
-  { name: "Projects", img: projektIkon },
-  { name: "Grafik", img: grafikIkon }
+  { name: "Me", img: meIcon, 
+    subs: [
+        {name: "About Me", img: placeholder},
+        {name: "Kontakt", img: placeholder}
+    ] },
+  { name: "SoMe", img: SoMeIkon, 
+    subs: [
+        {name: "LinkedIn", img: placeholder},
+        {name: "Github", img: placeholder}
+    ]
+  },
+  { name: "Projects", img: projektIkon,
+     subs: [
+        {name: "Project1", img: placeholder},
+        {name: "Project2", img: placeholder}
+    ]
+   },
+  { name: "Grafik", img: grafikIkon,
+     subs: [
+        {name: "Grafik1", img: placeholder},
+        {name: "Grafik2", img: placeholder}
+    ]
+  }
 ];
 export default categories;
--- src/components/TopCat.jsx
+++ src/components/TopCat.jsx
@@ -3,16 +3,31 @@ import { useState, useEffect } from "react";
 
 export default function TopCat() { 
     const [selectedIndex, SetSelectedIndex] = useState(0)
+    const [selectedSubIndex, SetSelectedSubIndex] = useState(0)
     
-    useEffect(() => { //Hvis jeg trykker på en key, og den er ArrowRight, så highlight den i næste index, medmindre index er større end eller = 3, så gå tilbage til nul
+     useEffect(() => { //Hvis jeg trykker på en key, og den er ArrowRight, så highlight den i næste index, medmindre index er større end eller = 3, så gå tilbage til nul
         function handleKeyDown(e) {
-            if (e.key === "ArrowRight") {
+            if (e.key === "ArrowDown") {
+                const subs = categories[selectedIndex].subs;
+                if(subs && subs.length > 0) //Kun hvis der er data, og det er mere end 1
+                SetSelectedSubIndex((prevIndex) =>
+                prevIndex >= subs.length -1 ? 0 : prevIndex + 1);
+            } else if (e.key === "ArrowUp") {
+                const subs = categories[selectedIndex].subs;
+                if(subs && subs.length >0)
+                SetSelectedSubIndex((prevIndex) =>
+                prevIndex === 0 ? subs.length -1 : prevIndex -1
+            );
+            }
+                if (e.key === "ArrowRight") {
                 SetSelectedIndex((prevIndex) =>
-                prevIndex > 3 -1 ? 0 : prevIndex + 1);
+                prevIndex > 3 -1 ? 0 : prevIndex + 1,
+            SetSelectedSubIndex(0));
+                
             } else if (e.key === "ArrowLeft") {
                 SetSelectedIndex((prevIndex) =>
-                prevIndex === 0 ? categories.length -1 : prevIndex -1
-            );
+                prevIndex === 0 ? categories.length -1 : prevIndex -1,
+                 SetSelectedSubIndex(0));
             }
         }
         window.addEventListener("keydown", handleKeyDown);
@@ -20,7 +35,9 @@ export default function TopCat() {
           return () => { //Fjern event listener når jeg er færdig
       window.removeEventListener("keydown", handleKeyDown);
     };
-    }, []);
+    }, [selectedIndex, selectedSubIndex]);
+    
+ 
 
     //Return functionen looper gennem categories, og mapper dem alle én af gangen
   return (
@@ -32,13 +49,24 @@ export default function TopCat() {
              
         {categories.map((category, index) => ( //categories mappes
         //Det gør den med en liste, som får en key
-        
-            <li key={index} className= {index === selectedIndex ? "top-highlight" : "category-item"}>
-                 <h1>{category.name}</h1>
+            
+            <li key={index} className= {index === selectedIndex ? "top-highlight" : "category-item"}
+            onClick={() => SetSelectedIndex(index)}>
+            
+                <h1>{category.name}</h1>
             <img src={category.img} alt={category.name + " icon"} />
-           
+            
             </li>
         ))}
+        </ul>
+        <ul className="subCategory">
+                    {categories[selectedIndex].subs.map((sub, index) => (
+        <li key={index} className={index === selectedSubIndex ? "sub-highlight" : "subCategory-item"}>
+            <p>{sub.name}</p>
+            <img src={sub.img} alt={sub.name + "icon"} />
+        </li>
+        ))}
+
         </ul>
     </div>
   );
