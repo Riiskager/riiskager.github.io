efb0aca9dabd34ea703563bf0d7ec477b451c1f2
Riiskager <emil.axel.riis@gmail.com>
2025-09-11 13:59:51 +0200

    en masse styling

--- src/sidePanels/Kontakt.jsx
+++ src/sidePanels/Kontakt.jsx
@@ -6,9 +6,10 @@ function Kontakt(){
             <img src={mig} alt='mig'></img>
             <article>
                 <h1> Kontakt </h1>
-                    <p>   Hvis du ønsker at kontakte mig, så kan jeg træffes på denne <a href="mailto:emil.axel.riis@gmail.com?subject=Question%20about%20Your%20Service">mail</a>
+                    <p>   Hvis du ønsker at kontakte mig, så kan jeg træffes på dette <a href="mailto:emil.axel.riis@gmail.com?subject=Job/Praktik%20mulighed%20[Indsæt navn]" target="_blank" rel="noopener noreferrer">link</a>
                 </p>
-                    
+                <h1>Alternativt</h1>
+                <p>Så er min Email: emil.axel.riis@gmail.com</p>
             </article>
         </div>
     )
