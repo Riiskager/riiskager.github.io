29586726f9a87cf30113ef97ace7ab3142f5aad7
Riiskager <emil.axel.riis@gmail.com>
2025-09-16 14:12:29 +0200

    mere fyld på paneler

--- src/sidePanels/GitHub.jsx
+++ src/sidePanels/GitHub.jsx
@@ -1,9 +1,9 @@
-import mig from '../assets/Mig.jpg'
+import GitHub from '../assets/Github.jpg'
 
 export default function Github(){
     return(
         <div className="GitHub">
-                    <img src={mig} alt='mig'></img>
+                    <img src={GitHub} alt='mig'></img>
                     <article>
                         <h1> <a href="https://github.com/Riiskager" target="_blank">Github</a> </h1>
                     </article>
--- src/sidePanels/ZeroBuzz.jsx
+++ src/sidePanels/ZeroBuzz.jsx
@@ -7,12 +7,17 @@ export default function ZeroBuzz(){
         <img src={ZeroLogo}></img>
             <article>
                 <a href="https://zerobuzzbrew.axelriis.dk/" target="blank"><h1>ZeroBuzzBrew</h1></a>
-                <p>ZeroBuzz Brew var et gruppeprojekt med fokus på en fiktiv alkoholfri øl-producent.
-                    <br />
-                    <br />
-                    
+                <p>ZeroBuzz Brew var et gruppeprojekt med fokus på en online webshop til en fiktiv alkoholfri øl-producent.
                 </p>
+                 <br />
                 <img src={ZeroBG}></img>
+                <p>Til udarbejdning af designprocesserne blev adobe pakken brugt i stor stil - Både illustrator, Dimensions og Aftereffects
+                    <br/> 
+                    <br />
+                    De elementer jeg specifikt fik ansvaret for i designprocessen var bl.a Logoprocessen, design af dåsen, samt forsiden og den tilhørenede animation.
+                </p>
+                <br />
+                
             </article>
         </div>
     )
