830bb6511994d03abbd3a053e6fea2ae3d732e10
Riiskager <emil.axel.riis@gmail.com>
2025-09-18 12:51:16 +0200

    Update Kogebog.jsx

--- src/sidePanels/Kogebog.jsx
+++ src/sidePanels/Kogebog.jsx
@@ -1,16 +1,16 @@
-import top from '../assets/ATKBB.png'
-import midt from '../assets/ATKB.png'
+import kogebogtop from '../assets/ATKBB.png'
+import kogebogmidt from '../assets/ATKB.png'
 
 export default function Kogebog(){
     return(
         <div className="Kogebog">
-        <img src={top}></img>
+        <img src={kogebogtop}></img>
             <article>
                 <a href="https://alletiders-kogebog.axelriis.dk/" target="blank"><h1>AlleTiders Kogebog</h1></a>
              <p>Dette projekt var et af de første store projekter på 1. semester, da ambitionerne var langt højere end hvad evnerne tilladte
                 <br/> Projektet pressede grænserne for hvad der var muligt, dengang, og gav en indsigt i hvor meget man kan opnå, så længe man er drevet nok
              </p>
-             <img src={midt}></img>
+             <img src={kogebogmidt}></img>
             </article>
         </div>
     )
