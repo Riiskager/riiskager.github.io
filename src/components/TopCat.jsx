647ac2dd82675896ee4be36bb945cec44c0fdb41
Riiskager <emil.axel.riis@gmail.com>
2026-02-19 13:51:35 +0100

    Komplet ændring af topcat

--- src/components/TopCat.jsx
+++ src/components/TopCat.jsx
@@ -1,5 +1,5 @@
 import categories from "../Data/Categories";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
@@ -12,24 +12,24 @@ export default function TopCat() {
 
      useEffect(() => { //Hvis jeg trykker på en key, og den er ArrowRight, så highlight den i næste index, medmindre index er større end eller = 3, så gå tilbage til nul
         function handleKeyDown(e) {
-            if (e.key === "ArrowDown") {
+            if (e.key === "ArrowRight") {
                 const subs = categories[selectedIndex].subs;
                 if(subs && subs.length > 0) //Kun hvis der er data, og det er mere end 1
                 SetSelectedSubIndex((prevIndex) =>
                 prevIndex >= subs.length -1 ? 0 : prevIndex + 1);
-            } else if (e.key === "ArrowUp") {
+            } else if (e.key === "ArrowLeft") {
                 const subs = categories[selectedIndex].subs;
                 if(subs && subs.length >0)
                 SetSelectedSubIndex((prevIndex) =>
                 prevIndex === 0 ? subs.length -1 : prevIndex -1
             );
             }
-                if (e.key === "ArrowRight") {
+                if (e.key === "ArrowDown") {
                 SetSelectedIndex((prevIndex) =>
                 prevIndex > 3 -1 ? 0 : prevIndex + 1,
             SetSelectedSubIndex(0));
                 
-            } else if (e.key === "ArrowLeft") {
+            } else if (e.key === "ArrowUp") {
                 SetSelectedIndex((prevIndex) =>
                 prevIndex === 0 ? categories.length -1 : prevIndex -1,
                  SetSelectedSubIndex(0));
@@ -42,16 +42,33 @@ export default function TopCat() {
     };
     }, [selectedIndex, selectedSubIndex]);
     
- 
+    const listRef = useRef(null);
+    const itemRefs = useRef([]);
+    const wrapperRef = useRef(null);
+       useEffect(() => {
+    const list = listRef.current;
+    const selectedItem = itemRefs.current[selectedIndex];
+    const container = wrapperRef.current;
+
+    if (!list || !selectedItem || !container) return;
+
+    const containerCenter = container.offsetHeight / 2;
+
+    const itemCenter = selectedItem.offsetTop + selectedItem.offsetHeight / 2;
+
+    const offset = containerCenter - itemCenter;
+
+    list.style.transform = `translateY(${offset}px)`;
+
+}, [selectedIndex]);
 
     //Return functionen looper gennem categories, og mapper dem alle én af gangen
   return (
     //listen laves
     <div className="whole-page">
-        <div className="carousel">
+        <div className="carousel" ref={wrapperRef} >
             
-            <ul className="top-categories" //Styrer hvor meget de bliver skubbet med når man vælger en ny overkategori (Problemer alt efter skærmstørrelse)
-            style={{ transform: `translateX(-${selectedIndex * 10}vw)`}}>
+            <ul className="top-categories" ref={listRef} >
                 
             {categories.map((category, index) => ( //categories mappes
             //Det gør den med en liste, som får en key
@@ -60,7 +77,7 @@ export default function TopCat() {
                 onClick={() => {
                     SetSelectedIndex(index) //Sørger for at man kan klikke på toppen, og når man gør så genstartet den underkategorierne
                     SetSelectedSubIndex(0)
-                }}>                
+                }}ref={el => itemRefs.current[index] = el}>                
                 
                 
                 <img src={category.img} alt={category.name + " icon"} />
@@ -70,7 +87,7 @@ export default function TopCat() {
             </ul>
             <div key={`subcat-${selectedIndex}`} className="subCategoryWrapper"> 
                 <ul className="subCategory" //Her er underkategorien! Fremvist kun! hvis den passer til den tilegnede overkategori
-                style={{ transform: `translateY(-${selectedSubIndex * 3.5}rem)`}}>
+                style={{ transform: `translateX(-${selectedSubIndex * 9}rem)`}}>
                             {categories[selectedIndex].subs.map((sub, index) => (
                 <li key={index} className={index === selectedSubIndex ? "sub-highlight" : "subCategory-item"}
                 onClick={() =>
